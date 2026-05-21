22c378e0ead60498e13d6b66e39c610be96281bc
Toan Nguyen <nguyentoan062411@gmail.com>
2026-05-21 08:23:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+  return (
+    <>
+    <div className="information"> 
+      <h1 className="Fullname">Nguyen The Toan</h1>
+      <p>Student ID: HE200136</p>
+      <p>Class: SE2004-NJ</p>
+      <p>Major: FER202</p>
+      <p>List hobbies:
+        <div className="hobbies">
+          <p>Reading</p>
+          <p>Swimming</p>
+          <p>Playing guitar</p>
+        </div>
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
